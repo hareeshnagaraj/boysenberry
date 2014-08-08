f4c8f97129eb575474633c0edc2363957f8a8d5c
Your Name <hareesh.nagaraj@gmail.com>
2014-08-08 14:18:21 -0400

    added jquery support in content script

--- boysenEvents.js
+++ boysenEvents.js
@@ -1,10 +1,14 @@
 /*
 boysenEvents.js 
 -Registers click of action icon
--Calls content script file for insertion of other elements/javascript
+-Calls content script boysenInsertions.js file for insertion of other elements/javascript
 */
 
 // Called when the user clicks on the browser action.
+//
 chrome.browserAction.onClicked.addListener(function(tab) {
-  window.alert('Welcome to Boysen');
+  
+  window.alert("Welcome to Boysen");
+  chrome.tabs.executeScript(null, {file: "boysenInsertions.js"});
+
 });
---
+++ boysenInsertions.js
@@ -0,0 +1,23 @@
+/*
+
+boysenInsertions.js
+-Add borders to all elements
+-Call script to add listener to every element, which then shows CSS for that element in a fixed position element
+-Capture mouse movement, and display x,y coordinates
+
+*/
+
+// chrome.tabs.executeScript({
+//     code: 'document.body.style.backgroundColor="red"'
+// });
+
+// var greeting = "hola, ";
+// var button = document.getElementById("toc");
+// button.person_name = "Roberto";
+// button.addEventListener("click", function() {
+//   alert(greeting + button.person_name + ".");
+// }, false);
+
+$(document).ready(function(){
+	window.alert("boysenInsertions");
+});
