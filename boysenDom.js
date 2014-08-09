793fa1db359878244e02bf1b96785dd8297f5a36
Dheeraj Manjunath <dheerajmanju1@yahoo.com>
2014-08-09 15:29:13 -0400

    clean linking

--- boysenDom.js
+++ boysenDom.js
@@ -3,7 +3,7 @@ Test of altering dom via a function
 
 boysenDom.js
 -Changes all elements of a dom
-	Add borders to all elements
+  Add borders to all elements
 
 TODO 
 Fix alignment issues in certain websites, arises when adding border of 1 px
@@ -14,35 +14,32 @@ Remove borders
 //parseDom used to parse the entire DOM and add the appropriate borders
 
 function parseDom(){
-	console.log("parseDom");
-	var all = document.getElementsByTagName("*");
-	this.red = "1px solid red";
-	this.blue = "1px solid blue";
-	this.purple = "1px solid purple";
-	for (var i=0, max=all.length; i < max; i++) {
-	     // Do something with the element here
-	     var currentElement = all[i];
-	     var currentId = currentElement.id;
-	     if(!currentId){
-	     	currentId = "boysenID"+i;
-	     }
-	     console.log(currentElement);
-	     console.log(currentId);
-	     //Adding the hover listener
+  console.log("parseDom");
+  var all = document.getElementsByTagName("*");
+  this.red = "1px solid red";
+  this.blue = "1px solid blue";
+  this.purple = "1px solid purple";
+  for (var i=0, max=all.length; i < max; i++) {
+    // Do something with the element here
+    var currentElement = all[i];
+    var currentId = currentElement.id;
+    if(!currentId){
+      currentId = "boysenID"+i;
+    }
+    console.log(currentElement);
+    console.log(currentId);
+    //Adding the hover listener
 
-	     var numChildren = currentElement.childNodes.length;
-	     
-	     if(numChildren > 3){
-	     	currentElement.style.border = this.red;
-	     }
-	     else if(numChildren > 2){
-	     	currentElement.style.border = this.blue;
-	     }
-	     else if(numChildren > 0){
-	     	currentElement.style.border = this.purple;
-	     }
-	}
+    var numChildren = currentElement.childNodes.length;
+     
+    if(numChildren > 3){
+      currentElement.style.border = this.red;
+    }
+    else if(numChildren > 2){
+      currentElement.style.border = this.blue;
+    }
+    else if(numChildren > 0){
+      currentElement.style.border = this.purple;
+    }
+  }
 }
-
-parseDom();
-
--- boysenInsertions.js
+++ boysenInsertions.js
@@ -9,20 +9,20 @@ boysenInsertions.js - content script
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 	if (request.action == "init"){
-	  console.log(request);
+    console.log(request);
 	}
 });
 
 /*	Listen for the keyboard shortcut */
 
 //’secret’ specifies the numerical keystrokes that make up the word “mario”
-var secret = "667989836978"; 
+var secret = "667989836978";
 var input = "";
 var timer;
 
 //The following function sets a timer that checks for user input. You can change the variation in how long the user has to input by changing the number in ‘setTimeout.’ In this case, it’s set for 500 milliseconds or ½ second.
 $(document).keyup(function(e) {
-   input += e.which;    
+   input += e.which;
    clearTimeout(timer);
    timer = setTimeout(function() { input = ""; }, 500);
    check_input();
@@ -31,10 +31,14 @@ $(document).keyup(function(e) {
 //Once the time is up, this function is run to see if the user’s input is the same as the secret code
 function check_input() {
     if(input == secret) {
-        //the code used to reveal mario and the world is then put here   
-        chrome.runtime.sendMessage({action: "boysen"}, function(response) {
-		  console.log(response.farewell);
-		});
+      //the code used to reveal mario and the world is then put here   
+      chrome.runtime.sendMessage({action: "boysen"}, function(response) {
+        console.log(response.farewell);
+      });
     }
-    console.log(input)
-};
+    console.log(input);
+}
+
+function alertMsg() {
+  console.log("somethign was clicked");
+}
