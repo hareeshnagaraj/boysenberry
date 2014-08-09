142ee7fb9bcd3fee64f43cf352ee44477434effe
Your Name <hareesh.nagaraj@gmail.com>
2014-08-09 19:12:39 -0400

    got the box to update!!

--- boysenInsertions.js
+++ boysenInsertions.js
@@ -6,7 +6,7 @@ boysenInsertions.js - content script
 -Capture mouse movement, and display x,y coordinates
 
 */
-var currentElement;
+var currentElement = "";
 
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
@@ -57,11 +57,82 @@ function alertMsg() {
 Adds the box to our page
 */
 function appendBox(){
-  var box = '<div class="popupWrap"><div class="popupTitle"><div class="popupTitleInner">boysenberry</div><div id="search_icon" class="popupTitleDisplayModeToggle">toggle view</div></div><div class="popupClassDisplay"><div class="popupClassDisplayOuter">.class {</div><div class="popupClassDisplayBody">width:400px;<br>height:500px;<br>background-color: rgba(241,241,241,0.8);<br>right:20px;<br>top:20px;<br>display: none;<br>overflow-x:hidden; <br></div><div class="popupClassDisplayOuter">}</div></div></div>';
+  var box = '<div class="popupWrap"><div class="popupTitle"><div class="popupTitleInner">boysenberry</div><div id="search_icon" class="popupTitleDisplayModeToggle">toggle view</div></div><div class="popupClassDisplay"><div id="boxClassName" class="popupClassDisplayOuter">.class {</div><div id="boxClassBody" class="popupClassDisplayBody">width:400px;<br>height:500px;<br>background-color: rgba(241,241,241,0.8);<br>right:20px;<br>top:20px;<br>display: none;<br>overflow-x:hidden; <br></div><div class="popupClassDisplayOuter">}</div></div></div>';
   $("body").prepend(box);
 }
 
+/*
+Updates the box according to which element we are hovering over
+*/
 function updateBox(element){
-  console.log(element);
+  if(element){
+    var elementID;
+    if(!element.id){
+      element.id = "boysen"+Math.floor((Math.random() * 10000) + 1);
+    }
+    elementID = element.id;
+    var accessID = "#"+elementID;
+    if($(accessID) != null){
+      var className = $(accessID).attr('class');
+      var classCSS = $(accessID).getStyleObject();
+      var css = grabStyle(classCSS);
+      if(!className){
+        className = "anonymous_class";
+      }
+      var classString = "."+className+" {";
+      if(className != currentElement){
+        $("#boxClassName").html(classString);
+        currentElement = className;
+      }
+      $("#boxClassBody").html(css);
+    }
+  }
 }
+
+function grabStyle(styleObject){
+  var string = "";
+  for (var key in styleObject) {
+    var val = styleObject[key];
+    string += key + ":" + val + "<br>";
+  }
+  return string;
+}
+
+/*
+ * getStyleObject Plugin for jQuery JavaScript Library
+ * From: http://upshots.org/?p=112
+ *
+ * Copyright: Unknown, see source link
+ * Plugin version by Dakota Schneider (http://hackthetruth.org)
+ */
+
+(function($){
+    $.fn.getStyleObject = function(){
+        var dom = this.get(0);
+        var style;
+        var returns = {};
+        if(window.getComputedStyle){
+            var camelize = function(a,b){
+                return b.toUpperCase();
+            }
+            style = window.getComputedStyle(dom, null);
+            for(var i=0;i<style.length;i++){
+                var prop = style[i];
+                var camel = prop.replace(/\-([a-z])/g, camelize);
+                var val = style.getPropertyValue(prop);
+                returns[camel] = val;
+            }
+            return returns;
+        }
+        if(dom.currentStyle){
+            style = dom.currentStyle;
+            for(var prop in style){
+                returns[prop] = style[prop];
+            }
+            return returns;
+        }
+        return this.css();
+    }
+})(jQuery);
+
 
