5fda4b9b134e13de6380d9394ff95d6d510f5155
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2014-08-11 22:04:08 -0400

    added CMD

--- boysenInsertions.js
+++ boysenInsertions.js
@@ -114,9 +114,9 @@ function appendBox(){
               </div> \
               <div class="popupClassDisplayOuter"> \
                 <h3 style="font-weight: bold;">Keyboard Shortcuts</h3> \
-                <div>Ctrl + B to show/hide application</div> \
+                <div>Ctrl/Cmd + B to show/hide application</div> \
                 <div>Alt/Option + B to copy</div> \
-                <div>Ctrl + I to show/hide details</div> \
+                <div>Ctrl/Cmd + I to show/hide details</div> \
               </div> \
             </div> \
             ';
